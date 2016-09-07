1d3557bbd73564edc3b41981a7a008cd62efa27b
Adriano Wichmann <estudoalegre@gmail.com>
2016-09-07 16:25:35 -0300

    criando repositoris

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,9 +3,12 @@
 namespace CodeDelivery\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
 
-class Client extends Model
+class Client extends Model implements Transformable
 {
+    use TransformableTrait;
 
     protected $fillable = [
         'user_id',
@@ -19,4 +22,5 @@ class Client extends Model
     public function user(){
         return $this->hasOne(\CodeDelivery\Models\User::class);
     }
+
 }
